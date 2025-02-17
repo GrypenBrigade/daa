6b6e8467268a95cd1219a1a8b6c47684fb344c90
GrypenBrigade <robloxbullcrap@gmail.com>
2025-02-17 20:03:41 +0800

    Update singly

--- LinkedList/Singly.java
+++ LinkedList/Singly.java
@@ -39,32 +39,135 @@ public class Singly {
         return false; // Return false if the specific value does not 
     }
 
-    public static void length() { // Method that 
+    public static int length(Node head) { // Method that measures the length of the list
+        int length = 0; // Initialization
+        Node current = head; // Start from the start
+        while(current != null) { // Keep running until the end of the list
+            length++; // Add length value
+            current.next = current; // Move to the next one
+        }
+        return length; // Return final length
 
     }
 
-    public static void insertAtStart() {
-
+    public Node insertAtStart(Node head, int value) { // Method that inserts a node at the start
+        Node newNode = new Node(value); // Create new node with given value
+        newNode.next = head; // Set new node pointer to current head
+        head = newNode; // Make the new node to become the head 
+        return head; // Return new node
     }
 
-    public static void insertAtEnd() {
+    public Node insertAtEnd(Node head, int value) {
+        Node newNode = new Node(value); // Create new node with given value
 
+        if (head == null) { // If list is empty, make new node the head
+            return newNode;
+        }
+
+        Node current = head; 
+        while (current.next != null){ // Traverse list until the end
+            current = current.next;
+        }
+
+        current.next = newNode; // Link new node to the last node
+        return head; // Return new node
     }
 
-    public static void insertAtPos() {
+    public static Node insertAtPos(Node head, int pos, int value) { // Method that inserts in to a specific position
+        if (pos < 1) { // Check if position is valid
+            System.out.println("Not a valid position");
+            return head;
+        }
 
+        if (pos == 1) { // If positions is the start of the list
+            Node newNode = new Node(value);
+            newNode.next = head;
+            return newNode;
+        }
+
+        Node prevNode = head;
+        int count = 1;
+        while (count < pos - 1 && prevNode != null) { // Traverse the list until specificed position
+            prevNode = prevNode.next;
+            count++;
+        }
+
+        if (prevNode == null) { // If specified position is greater than the number of nodes
+            System.out.println("Not a valid position");
+            return head;
+        }
+
+        // Insert new node from specified position
+        Node newNode = new Node(value);
+        newNode.next = prevNode.next;
+        prevNode.next = newNode;
+
+        return head; // Return new node
     }
 
-    public static void deleteStart() {
+    public static Node deleteStart(Node head) { // Method that removes the starting node
+        if (head == null) { // Return null if list is empty
+            return null;
+        }
 
+        Node tempNode = head; // Move first node to temporary node
+        head = head.next; // Move first node 
+        tempNode = null; // Nullfiy temporary node
+
+        return head; // Return new first node
     }
 
-    public static void deleteEnd() {
+    public static Node deleteEnd(Node head) { // Method that removes the ending node
+        if (head == null) { // Return null if list is empty
+            return null;
+        }
 
+        if (head.next == null) { // If list is just node, delete it
+            head = null;
+            return null;
+        }
+
+        
+        Node secondLastNode = head; // Find the second to the last node
+        while (secondLastNode.next.next != null) {
+            secondLastNode = secondLastNode.next;
+        }
+
+        
+        secondLastNode.next = null; // Remove the last node
+        return head; // Return new last node
     }
 
-    public static void deletePos() {
+    public static void deletePos(Node head, int pos) { // Method that deletes the node of a given postion
+        if (head == null || pos < 1) { // Check
+            System.out.println("Not a valid position");
+            return;
+        }
 
+        if (pos == 1) { // If position is 1, delete it
+            Node tempNode = head; // Create temporary node
+            head = head.next; // Move node to another node
+            tempNode = null; // Nullify tempNode
+            return;
+        }
+
+        Node currentNode = head; // Traverse the list to before the desired node to deleted
+        for (int z = 1; z < pos - 1 && currentNode != null; z++) {
+            currentNode = currentNode.next;
+        }
+
+        if (currentNode == null || currentNode == null) { // If position is out of range of the list
+            System.out.println("Not a valid position");
+            return;
+        }
+
+        Node tempNode = currentNode.next; // Temporary node
+        currentNode.next = currentNode.next.next; // Update nodes to bypass the deleted node
+        tempNode = null; // Delete node
+    }
+
+    public static void main(String[] args) {
+        
     }
 
 
