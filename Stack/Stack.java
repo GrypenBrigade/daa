194a7d659da90810e26d76964eeb180ce921e8f3
GrypenBrigade <robloxbullcrap@gmail.com>
2025-02-17 22:13:40 +0800

    Stack

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -1,5 +1,29 @@
 package Stack;
 
+class Node {
+    int val;
+    Node top;
+
+    Node(int val) {
+        this.val = val;
+        this.top = null;
+    }
+}
+
 public class Stack {
-    
+    private Node head;
+
+    Stack() {
+        this.head = null;
+    }
+
+    boolean empty() {
+        return head == null;
+    }
+
+    void push(int val) {
+        Node newStack = new Node(val);
+        newStack.top = head;
+        head = newStack;
+    }
 }
