81491ed19430a57b9b693b3993a1478ec881c44b
GrypenBrigade <robloxbullcrap@gmail.com>
2025-02-17 23:13:57 +0800

    Updated

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -2,28 +2,59 @@ package Stack;
 
 class Node {
     int val;
-    Node top;
+    Node next;
 
     Node(int val) {
         this.val = val;
-        this.top = null;
+        this.next = null;
     }
 }
 
 public class Stack {
     private Node head;
 
-    Stack() {
+    public Stack() {
         this.head = null;
     }
 
-    boolean empty() {
+    public boolean empty() {
         return head == null;
     }
 
-    void push(int val) {
+    public void push(int val) {
         Node newStack = new Node(val);
-        newStack.top = head;
+        newStack.next = head;
         head = newStack;
+    }
+
+    public void pop() {
+        if(empty()){
+            System.out.println("Stack underflow");
+        }
+        else {
+            Node tempNode = head;
+            head = head.next;
+            tempNode = null;
+        }
+    }
+
+    public int peek() {
+        if (!empty()) {
+            return head.val;
+        }
+        else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+        stack.push(10);
+        stack.push(11);
+        stack.push(12);
+
+        System.out.print(stack.peek());
     }
 }
