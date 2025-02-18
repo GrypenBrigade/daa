30e69eb61081a37ab5a777fac861ea07fd4fd2c6
GrypenBrigade <robloxbullcrap@gmail.com>
2025-02-18 18:44:03 +0800

    Doubly

--- LinkedList/Doubly.java
+++ LinkedList/Doubly.java
@@ -1,5 +1,31 @@
 package LinkedList;
 
+class Node {
+    int val;
+    Node prev;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+
+    }
+    
+}
+
 public class Doubly {
+    public void forwTraversal(Node head){
+        Node currentNode = head;
+
+        while(currentNode != head) {
+            
+        }
+    }
+
+    public void backTraversal(Node tail) {
+        Node currentNode = tail;
+    }
+
     
 }
