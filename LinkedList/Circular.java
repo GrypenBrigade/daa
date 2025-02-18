a9dad87a4d44a161f6047f3d6c6d5c282b2fa835
GrypenBrigade <robloxbullcrap@gmail.com>
2025-02-19 00:33:25 +0800

    Circular

--- LinkedList/Circular.java
+++ LinkedList/Circular.java
@@ -1,5 +1,15 @@
 package LinkedList;
 
+class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 public class Circular {
     
 }
