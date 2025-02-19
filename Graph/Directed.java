a897ded0bb8509039e1335d512c4dfb85c9d8435
GrypenBrigade <robloxbullcrap@gmail.com>
2025-02-19 23:32:45 +0800

    main() updated

--- Graph/Directed.java
+++ Graph/Directed.java
@@ -1,5 +1,58 @@
 package Graph;
 
-public class Directed {
-    
+import java.util.*;
+
+class DirectedGraph {
+    private Map<Integer, List<Integer>> adjList;
+
+    public DirectedGraph() {
+        adjList = new HashMap<>();
+    }
+
+    public void addVertex(int vertex) {
+        adjList.putIfAbsent(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(int from, int to) {
+        adjList.putIfAbsent(from, new ArrayList<>());
+        adjList.get(from).add(to);
+    }
+
+    public void bfs(int start) {
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        
+        queue.add(start);
+        visited.add(start);
+
+        System.out.print("BFS Traversal: ");
+        
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int neighbor : adjList.getOrDefault(node, new ArrayList<>())) {
+                if (!visited.contains(neighbor)) {
+                    queue.add(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DirectedGraph graph = new DirectedGraph();
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+
+        graph.bfs(1); // Expected output: 1 2 3 4
+    }
 }
